caf843ebb3232b4ab7dc95ef9aa5d53404b42bca
gun5880 <gun5880@naver.com>
2020-08-27 18:48:57 +0900

    비밀정 보호

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gun5880:go154154@geon.9txca.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
 }).then(()=> console.log('mongodb connect'))
 .catch(err => console.log(err))
 
 
-app.get('/',(req,res)=>res.send('Hello Worlds'))
+app.get('/',(req,res)=>res.send('Hello Worlds~~~'))
+
+app.post('/register',(req,res)=>{
+
+    //회원가입 시 필요한 정보 -> DB
+    
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success : false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+
+})
+
 app.listen(port,()=> console.log('example app on port ${port}'))
